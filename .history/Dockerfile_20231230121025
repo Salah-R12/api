334b4dadf112003295eca5577897e6d8e374d9c7
Salah <salah@MacBook-Pro-de-Salah.local>
2023-12-31 13:50:21 +0100

    Initial commit of my API project

---
+++ .history/Dockerfile_20231230121025
@@ -0,0 +1,40 @@
+# Utiliser une image PHP officielle comme base
+FROM php:8.2-apache
+
+# Mettre à jour les paquets et installer les extensions PHP nécessaires
+RUN apt-get update && apt-get install -y \
+        libpq-dev \
+    && docker-php-ext-install \
+        pdo pdo_pgsql
+RUN apt-get install -y nano vim
+
+# Installer Composer
+RUN php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');" \
+    && php composer-setup.php \
+    && php -r "unlink('composer-setup.php');" \
+    && mv composer.phar /usr/local/bin/composer
+
+RUN sed -i '/<\/VirtualHost>/i\<Directory /var/www/html/public>\n\tAllowOverride All\n</Directory>' /etc/apache2/sites-available/000-default.conf && \
+    sed -i 's|DocumentRoot /var/www/html|DocumentRoot /var/www/html/public|' /etc/apache2/sites-available/000-default.conf
+
+# Installer les outils nécessaires
+RUN apt-get update && apt-get install -y \
+    git \
+    zip \
+    unzip
+
+# Activer le module mod_rewrite d'Apache
+RUN a2enmod rewrite
+
+# Copier les fichiers du projet dans le conteneur
+COPY . /var/www/html
+
+# Modifier la configuration d'Apache pour autoriser .htaccess
+# Ajouter la directive AllowOverride pour le répertoire public
+RUN echo '<Directory /var/www/html/public>\n\tAllowOverride All\n</Directory>' >> /etc/apache2/sites-available/000-default.conf
+
+# Définir les permissions des dossiers
+RUN chown -R www-data:www-data /var/www/html
+
+# Exposer le port 80
+EXPOSE 80
---
+++ .history/src/Entity/Book_20231228213319.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\BookRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource(
+ *     collectionOperations={"get", "post"},
+ *     itemOperations={"get", "put", "delete"}
+ * )
+ * @ORM\Entity
+ */
+#[ORM\Entity(repositoryClass: BookRepository::class)]
+class Book
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $author = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $isbn = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $publishedDate = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(string $author): static
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getIsbn(): ?string
+    {
+        return $this->isbn;
+    }
+
+    public function setIsbn(string $isbn): static
+    {
+        $this->isbn = $isbn;
+
+        return $this;
+    }
+
+    public function getPublishedDate(): ?\DateTimeInterface
+    {
+        return $this->publishedDate;
+    }
+
+    public function setPublishedDate(?\DateTimeInterface $publishedDate): static
+    {
+        $this->publishedDate = $publishedDate;
+
+        return $this;
+    }
+}
