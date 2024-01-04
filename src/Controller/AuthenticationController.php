599e7e604e1ee0636ecd8ff5d4087453f27f6193
Salah <salah@MacBook-Pro-de-Salah.local>
2024-01-04 21:59:13 +0100

    Implement JWT Authentication for Secure API Access

---
+++ src/Controller/AuthenticationController.php
@@ -0,0 +1,17 @@
+<?php
+// src/Controller/AuthenticationController.php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AuthenticationController
+{
+    #[Route('/api/login_check', name: 'api_login_check', methods: ['POST'])]
+    public function login(): Response
+    {
+        // La logique d'authentification est gérée par le bundle JWT, cette méthode ne sera pas exécutée.
+        throw new \Exception('This method should not be called. This is handled by the "lexik_jwt_authentication" firewall.');
+    }
+}
